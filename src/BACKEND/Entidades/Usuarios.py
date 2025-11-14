af27cb7eedfc576d8b755168bab76087f4d4a6c9
zarate <232310065@itslerdo.edu.mx>
2025-11-14 17:44:58 -0600

    Entidades Usuarios

---
+++ src/BACKEND/Entidades/Usuarios.py
@@ -0,0 +1,10 @@
+
+class Usuarios:
+    def __init__(self, id, nombre, ocupacion, meta_mensual, fecha_registro, email, password):
+        
+        self.id: id = int
+        self.nombre: nombre = str
+        self.ocupacion: ocupacion = str 
+        self.meta_mensual: meta_mensual = float
+        
+    
